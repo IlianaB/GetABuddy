ce7a465d45316cf656f39b1b2e8a9c05eee1198a
Iliana Bobeva <iliana.bobeva@gmail.com>
2016-02-21 10:48:39 +0200

    Create CommentViewModel

---
+++ Source/Web/GetABuddy.Web/ViewModels/EventDetails/CommentViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace MvcTemplate.Web.ViewModels.EventDetails
+{
+    using AutoMapper;
+    using GetABuddy.Data.Models;
+    using GetABuddy.Web.Infrastructure.Mapping;
+
+    public class CommentViewModel : IMapFrom<Comment>, IHaveCustomMappings
+    {
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public string Event { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<Comment, CommentViewModel>()
+                .ForMember(x => x.Author, opt => opt.MapFrom(x => x.Author.UserName));
+
+            configuration.CreateMap<Comment, CommentViewModel>()
+                .ForMember(x => x.Event, opt => opt.MapFrom(x => x.Event.Name));
+        }
+    }
+}
