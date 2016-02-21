f8a2c34e84b9c7bf205e4a1811b9d0a7eb162b99
Iliana Bobeva <iliana.bobeva@gmail.com>
2016-02-21 16:31:35 +0200

    Create category controller

---
+++ Source/Web/GetABuddy.Web/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿namespace GetABuddy.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    using Services.Data;
+
+    public class CategoryController : BaseController
+    {
+        private readonly ICategoriesService categories;
+
+        public CategoryController(ICategoriesService categories)
+        {
+            this.categories = categories;
+        }
+
+        public ActionResult All()
+        {
+            var categories = this.categories.GetAll();
+            var viewModel = this.Mapper.Map<EventDetailsViewModel>(categories);
+
+            return this.View(viewModel);
+        }
+
+        public ActionResult ById(string id)
+        {
+            var eventById = this.events.GetById(id);
+            var viewModel = this.Mapper.Map<EventDetailsViewModel>(eventById);
+
+            return this.View(viewModel);
+        }
+    }
+}
