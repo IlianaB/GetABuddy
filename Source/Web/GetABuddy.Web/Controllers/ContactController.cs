1413b051208e4858f74cd23e7473d7198ae9817c
Iliana Bobeva <iliana.bobeva@gmail.com>
2016-02-23 22:53:54 +0200

    Add About and Contact pages

---
+++ Source/Web/GetABuddy.Web/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿namespace GetABuddy.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class ContactController : Controller
+    {
+        public ActionResult Info()
+        {
+            return this.View();
+        }
+    }
+}
